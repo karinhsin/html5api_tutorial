d35ee8ab2647d96ced22e9a7e3c65ee588db86a9
Karin Hsin <karinhsin@gmail.com>
2021-09-23 09:51:10 +0800

    修改聊天室Server端

--- routes/ws-chat.js
+++ routes/ws-chat.js
@@ -2,20 +2,26 @@ const WebSocket = require('ws');
 
 const createChatServer = server => {
     const wsServer = new WebSocket.Server({ server });
-    const map = new Map();
+    const map = new Map(); //存放對應的名稱
 
     wsServer.on('connection', (ws, req) => {
-        console.log('size:', wsServer.clients.size);
-        console.log('ip:', req.connection.remoteAddress);
-
+        console.log(wsServer.clients); //set類型
+        map.set(ws, { name: '' }); //設定對應的物件 進到聊天室的暱稱 先給空字串 用ws(WebSocket)的物件當key 
         ws.on('message', (message, isBinary) => {
-            // ws v8 多了 isBinary 參數
-            console.log('isBinary:', isBinary);
-            console.log(message, message.toString());
-            ws.send(message.toString());
+            const mObj = map.get(ws); //取得對應的物件
+            let msg; //要廣播的訊息(雖然說是廣播但其實是個別發)
+            if(!mObj.name){ //如果沒有名字
+                mObj.name = message.toString();  //把他傳進來的訊息當做他的名字
+                msf = `${mObj.name} 進到聊天室，共${wsServer.clients.size}人`;
+            }else{
+                msg = `${mObj.name}: ${message.toString()}`;
+            }
+            wsServer.clients.forEach(c => {
+                if (c.readyState === WebSocket.OPEN) {
+                    c.send(msg); //如果是開啟的狀態就發送剛剛準備好的訊息
+                }
+            });
         });
-
-        ws.send('連線了！');
     });
 };
 
